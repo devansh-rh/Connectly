086b2cb5d80da65d7a8a4e3a7a2ed727e6164b4a
Devansh <devanshrohilla9876@gmail.com>
2026-03-25 00:11:29 +0530

    Normalize CORS origins to avoid URL mismatch on deploy

--- server/constants/config.js
+++ server/constants/config.js
@@ -1,13 +1,24 @@
+const normalizeOrigin = (value = "") => value.trim().replace(/\/+$/, "");
+
+const configuredClientOrigins = [
+  process.env.CLIENT_URL,
+  ...(process.env.CLIENT_URLS || "").split(","),
+]
+  .map((origin) => normalizeOrigin(origin || ""))
+  .filter(Boolean);
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:4173",
+  ...configuredClientOrigins,
+];
+
 const corsOptions = {
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:4173",
-      process.env.CLIENT_URL,
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  };
-  
-  const CONNECTLY_TOKEN = "connectly-token";
-  
-  export { corsOptions, CONNECTLY_TOKEN };
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+};
+
+const CONNECTLY_TOKEN = "connectly-token";
+
+export { corsOptions, CONNECTLY_TOKEN };
